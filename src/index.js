d984970f1139cd2444222512647ee2f8c585d197
marlonbarrios <marlon@dance-tech.net>
2022-02-16 18:53:48 -0500

    search bod for user and clear

--- src/index.js
+++ src/index.js
@@ -1,25 +1,43 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import ReactDOM from "react-dom";
 
-const endPoint =  'https://api.github.com/users/marlonbarrios';
-
+const baseUrl = "https://api.github.com/users/";
 
 function App() {
-    const [user, setUser] = useState(null);
+  const [username, setUsername] = React.useState("codeartistryio");
+  const [user, setUser] = React.useState(null);
+  const searchInput = React.useRef();
 
+  React.useEffect(() => {
+    getUser();
+    // fetch(endpoint)
+    //   .then(response => response.json())
+    //   .then(data => setUser(data));
+  }, [ ]);
 
+  function handleClearInput() {
+    searchInput.current.value = "";
+    searchInput.current.focus();
+  }
 
-    useEffect(() => { 
-        async function getUser() {
-            const response = await fetch(endPoint);
-            const data = await response.json();
-            setUser(data);
-            }
-            getUser()
-    }, []);
+  async function getUser() {
+    const response = await fetch(`${baseUrl}${username}`);
+    const data = await response.json();
+    setUser(data);
+  }
 
  
-  return user ? (
+  return  (
+<div>
+    <input
+    type="text"
+    placeholder="Input username"
+    onChange={event => setUsername(event.target.value)}
+    ref={searchInput}
+  />
+  <button onClick={() => getUser(username)}>Search</button>
+  <button onClick={handleClearInput}>Clear</button>
+  {user ? (
     <div>
         <h2>{user.name}</h2>
         <p>{user.bio}</p>
@@ -29,8 +47,12 @@ function App() {
      <img alt='avatar' src={user.avatar_url} style={{height:100}}/>
         
     </div>
-  ) : ( <p>loading</p>);
+  ) : ( <p>loading</p>
+  )}
+</div>
+  );
 }
+  
 
 const rootNode = document.getElementById("root");
 ReactDOM.render(<App />, rootNode);
